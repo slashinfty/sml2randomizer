8f43fa5c294b7c41ecdb8fc1b07c59ea2999b696
slashinfty <matt.braddock@gmail.com>
2019-07-17 12:51:46 -0400

    fixing setting

--- js/interface.js
+++ js/interface.js
@@ -27,13 +27,13 @@ $('input:checkbox').change(function() {
             if ($('#f').is(':checked')) $('#text-f').toggleClass('hide');
             $('#f').prop('checked', false);
             break;
-		case '0':
+		case 'w':
 			if (!$('#l').is(':checked')) $('#text-l').toggleClass('hide');
 			$('#l').prop('checked', true);
 			break;
 		case 'l':
-			if ($('#0').is(':checked')) $('#text-0').toggleClass('hide');
-			$('#0').prop('checked', false);
+			if ($('#w').is(':checked')) $('#text-w').toggleClass('hide');
+			$('#w').prop('checked', false);
     }
 });
 
@@ -73,11 +73,11 @@ var cleanFlags = flagSubmit => {
     let flagArray = flagSubmit.split('+');
     let flags = flagArray[0];
     //strip out extraneous characters
-    flags = flags.replace(/[^l0bDdceupBgixXsfFmMho+]/g, '');
+    flags = flags.replace(/[^lwbDdceupBgixXsfFmMho+]/g, '');
     //strip out duplicate characters
     flags = flags.split('').filter((x, n, s) => s.indexOf(x) == n).join('');
     //disallow and strip out one-of settings
-	if (flags.includes('0') && !flags.includes('l')) flags = 'l' + flags;
+	if (flags.includes('w') && !flags.includes('l')) flags = 'l' + flags;
     if (flags.includes('D') && flags.includes('d')) flags = flags.replace(/[dD]/g, '');
     if (flags.includes('x') && flags.includes('X')) flags = flags.replace(/[xX]/g, '');
     if (flags.includes('f') && flags.includes('F')) flags = flags.replace(/[fF]/g, '');
@@ -88,7 +88,7 @@ var cleanFlags = flagSubmit => {
 
 //turning on checkboxes based on flags
 function setFlags(flags) {
-    let flagSet = ['l', '0', 'b', 'D', 'd', 'c', 'e', 'u', 'p', 'B', 'g', 'i', 'x', 'X', 's', 'f', 'F', 'm', 'M', 'h', 'o'], patchSet = ['dx'];
+    let flagSet = ['l', 'w', 'b', 'D', 'd', 'c', 'e', 'u', 'p', 'B', 'g', 'i', 'x', 'X', 's', 'f', 'F', 'm', 'M', 'h', 'o'], patchSet = ['dx'];
     let flagArray = flags.split('+');
     flagSet.forEach(letter => { 
         if (flagArray[0].includes(letter)) {
@@ -142,7 +142,7 @@ $('#copyLink').click(function() {
 var settings = () => {
     let flags = '';
     if ($('#l').is(':checked')) { doLevels = true; flags += 'l'; }
-	if ($('#y').is(':checked')) { doIncludeDuals = true; flags += '0'; }
+	if ($('#w').is(':checked')) { doIncludeDuals = true; flags += 'w'; }
     if ($('#b').is(':checked')) { doBosses = true; flags += 'b'; }
     if ($('#D').is(':checked')) { doAllDuals = true; flags += 'D'; }
     if ($('#d').is(':checked')) { doRandomDuals = true; flags += 'd'; }
